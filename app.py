4100ca0e0bbded15afeb5302715a505a47e3e1f5
Abdullah-hmed <abdullahwyi@gmail.com>
2024-08-26 02:16:01 +0500

    Error Handling Notifications Implemented using HTMX

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, send_from_directory, url_for, render_template_string
 import os
 import time
-
+# AI Imports
+# from StyleGAN_Pipeline.encode import ImageEncoder
+# from StyleGAN_Pipeline.generate import ImageGenerator
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads'
@@ -17,19 +19,18 @@ def front_page():
 def upload_image():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return "no file received", 400
+            return '<div class="notification is-danger" remove-me="1s">No File Received</div>', 422
         image = request.files['file']
         if not is_valid_file_type(image.filename):
-            return "invalid file type", 400
+            return '<div class="notification is-danger" remove-me="1s">Invalid File Type</div>', 422
         if image.filename == '':
-            return "no  file recived", 400
+            return '<div class="notification is-danger" remove-me="1s">No File Received</div>', 422
         if image:
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(image_path)
-            time.sleep(2)
+            time.sleep(1)
             return render_template_string("""<img src="{{ url_for('static', filename='uploads/' + filename) }}" alt="Uploaded image">""", filename=image.filename)
-    return "Invalid Request", 400
-    
+        return '<div class="notification is-danger" remove-me="1s">Invalid Request</div>', 422
 
 def is_valid_file_type(file):
     if file.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
