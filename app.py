a4d345f803b937e69cc533e10dc280777ca032f1
Abdullah-hmed <abdullahwyi@gmail.com>
2024-08-25 16:55:05 +0500

    Working Upload Button with loader

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, send_from_directory, url_for
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for, render_template_string
 import os
+import time
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'static/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -15,19 +16,26 @@ def front_page():
 @app.route('/upload', methods=['POST'])
 def upload_image():
     if request.method == 'POST':
-        # print(request.files.keys())
         if 'file' not in request.files:
             return "no file received", 400
         image = request.files['file']
+        if not is_valid_file_type(image.filename):
+            return "invalid file type", 400
         if image.filename == '':
             return "no  file recived", 400
         if image:
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(image_path)
-            return "Thanks, file received successfully"
+            time.sleep(2)
+            return render_template_string("""<img src="{{ url_for('static', filename='uploads/' + filename) }}" alt="Uploaded image">""", filename=image.filename)
     return "Invalid Request", 400
     
-# TODO: Display Image in DOM
+
+def is_valid_file_type(file):
+    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     app.run(debug=True)
